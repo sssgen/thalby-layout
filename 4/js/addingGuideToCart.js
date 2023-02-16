e4be4ca75c81b4f7badcf7322f126bc7f59de341
sssgen <118835027+sssgen@users.noreply.github.com>
2023-02-16 18:31:36 +0300

    Add files via upload

---
+++ 4/js/addingGuideToCart.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const societyCard =`
+	<div class="card product3">
+          <div class="face face1">
+            <div class="content">
+              <i class="fab fa-windows"></i>
+              <h3>Society Guides</h3>
+            </div>
+          </div>
+          <div class="face face2">
+            <div class="content">
+              <p>Our guides to society deliver all the social insight you need, helping you understand how people live, love and play today and why communities are the way they are.</p>
+            </div>
+          </div>
+    </div>
+`;
